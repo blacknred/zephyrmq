42000caf8adecf17f336f46af7140913e40a2e03
blcknrd <tretyakov.dmi@gmail.com>
2025-05-03 21:07:28 +0300

    4

--- message_broker.ts
+++ message_broker.ts
@@ -1,59 +1,56 @@
 import { LinkedListQueue } from "../queue";
-import { Logger, wait } from "../utils";
+import { Logger, wait } from "./utils";
 
+type ILogger = {
+  log(message: string, level: "INFO" | "WARN" | "DANGER"): void;
+};
 type IQueue<Data = any> = {
   enqueue(data: Data, priority?: number): void;
   dequeue(): Data | undefined;
-  peak(): Data | undefined;
+  peek(): Data | undefined;
   isEmpty(): boolean;
   size(): number;
 };
-class InMemoryQueue extends LinkedListQueue implements IQueue {}
+class InMemoryQueue extends LinkedListQueue implements IQueue {
+  // HeapQueue is 3-4x faster at scale due to O(log n) vs. O(n) inserts
+  // LinkedListQueue struggles with priorities (sorts on every insert).
+  // Memory usage is similar (~10MB for 1M messages).
+}
 
 type ICorrelationRegistry = {
   has(correlationId: string): boolean;
   get(correlationId: string): string | undefined;
-  set(correlationId: string, consumerId: string): void;
+  set(correlationId: string, consumerId: string): ICorrelationRegistry;
   delete(consumerId: string): boolean;
 };
 class InMemoryCorrelationRegistry
   extends Map<string, string>
   implements ICorrelationRegistry
 {
+  private timestamps = new Map<string, number>();
+  constructor(private ttl = 60_000) {
+    super();
+  }
+
+  set(correlationId: string, consumerId: string) {
+    super.set(correlationId, consumerId);
+    this.timestamps.set(correlationId, Date.now() + this.ttl);
+    return this;
+  }
+
+  get(correlationId: string) {
+    if (this.timestamps.get(correlationId)! < Date.now()) {
+      this.delete(correlationId);
+      return undefined;
+    }
+    return super.get(correlationId);
+  }
+
   delete(key: string): boolean {
     this.forEach((consumerIdx, correlationId) => {
       if (consumerIdx === key) super.delete(correlationId);
     });
     return true;
-  }
-}
-
-type IMessageMetadata = Partial<{
-  correlationId: string;
-  priority: number;
-  ttl: number;
-  ttd: number;
-  attempts: number;
-  timestamp: number;
-}>;
-
-class Message<Data = any> {
-  constructor(public data: Data, public metadata: IMessageMetadata) {}
-  static encode<Data>(message: Message<Data>) {
-    try {
-      return new TextEncoder().encode(JSON.stringify(message));
-    } catch (e) {
-      throw new Error("Failed to encode message");
-    }
-  }
-
-  static decode<Data>(record: Uint8Array<ArrayBufferLike>) {
-    try {
-      const message = JSON.parse(new TextDecoder().decode(record));
-      return new Message<Data>(message.data, message.metadata);
-    } catch (e) {
-      throw new Error("Failed to decode message");
-    }
   }
 }
 
@@ -63,7 +60,12 @@ class Broker {
   private consumers: Record<string, Set<string>> = {};
   public deadLetterTopic = "dead-letter";
 
-  constructor(private Queue: new () => IQueue<Uint8Array<ArrayBufferLike>>) {
+  constructor(
+    private Queue: new () => IQueue<Uint8Array<ArrayBufferLike>>,
+    public correlations?: ICorrelationRegistry,
+    public logger?: ILogger,
+    public maxDeliveryAttempts = 10
+  ) {
     this.assertTopic(this.deadLetterTopic);
   }
 
@@ -83,73 +85,113 @@ class Broker {
     delete this.consumers[name];
   }
 
-  addProducer(producer: Producer) {
-    const { topic, id } = producer;
-    this.assertTopic(topic);
-    this.producers[topic].add(id);
+  // Consistent hashing
+  isNotCorrelate(correlationId: string, agentId: string) {
+    if (!this.correlations) return false;
+    const correlatedAgentId = this.correlations.get(correlationId);
+    if (correlatedAgentId && correlatedAgentId !== agentId) return true;
+    this.correlations.set(correlationId, agentId);
+    return false;
   }
 
-  removeProducer(producer: Producer) {
-    const { topic, id } = producer;
-    this.producers[topic].delete(id);
+  register(agent: Producer | Consumer) {
+    const { topic, id } = agent;
+    this.assertTopic(topic);
+    if (agent instanceof Producer) {
+      if (topic == this.deadLetterTopic) {
+        throw new Error("Invalid topic");
+      }
+      this.producers[topic].add(id);
+    } else {
+      this.consumers[topic].add(id);
+    }
+  }
+
+  unregister(agent: Producer | Consumer) {
+    const { topic, id } = agent;
+    if (agent instanceof Producer) {
+      this.producers[topic]?.delete(id);
+    } else {
+      this.consumers[topic]?.delete(id);
+      this.correlations?.delete(id);
+    }
     if (!this.producers[topic].size && !this.consumers[topic].size) {
       this.purgeTopic(topic);
     }
   }
+}
 
-  addConsumer(consumer: Consumer) {
-    const { topic, id } = consumer;
-    this.assertTopic(topic);
-    this.consumers[topic].add(id);
+type IMessageMetadata = Partial<{
+  correlationId: string;
+  priority: number;
+  ttl: number;
+  ttd: number;
+  attempts: number;
+  timestamp: number;
+}>;
+class Message<Data = any> {
+  constructor(public data: Data, public metadata: IMessageMetadata = {}) {
+    this.metadata.timestamp ||= Date.now();
+    this.metadata.attempts ??= 0;
+    this.metadata.priority ??= 0;
   }
-
-  removeConsumer(consumer: Consumer) {
-    const { topic, id } = consumer;
-    this.consumers[topic].delete(id);
-    if (!this.producers[topic].size && !this.consumers[topic].size) {
-      this.purgeTopic(topic);
+  static encode<Data>(message: Message<Data>) {
+    try {
+      return new TextEncoder().encode(JSON.stringify(message));
+    } catch (e) {
+      throw new Error("Failed to encode message");
     }
   }
 
-  // unsubscribe(subscription: Subscription) {
-  //   const { topic, id } = subscription;
-  //   if (!this.queues[topic]) throw new Error("Topic not found");
+  static decode<Data>(record: Uint8Array<ArrayBufferLike>) {
+    try {
+      const message = JSON.parse(new TextDecoder().decode(record));
+      return new Message<Data>(message.data, message.metadata);
+    } catch (e) {
+      throw new Error("Failed to decode message");
+    }
+  }
 
-  
-  //   if (this.subscriptions[topic]) {
-  //     this.subscriptions[topic].delete(id);
-  //   }
-  // }
+  isExpired() {
+    const { ttl, timestamp } = this.metadata;
+    if (!ttl) return false;
+    return timestamp! + ttl <= Date.now();
+  }
+
+  isDelayed() {
+    const { ttd, timestamp } = this.metadata;
+    if (!ttd) return false;
+    return timestamp! + ttd > Date.now();
+  }
+
+  isExceedAttempts(maxAttempts: number) {
+    const { attempts = 0, priority = 0 } = this.metadata;
+    if (attempts <= maxAttempts) return false;
+    this.metadata.priority = priority - 1;
+    this.metadata.attempts = attempts + 1;
+    return true;
+  }
 }
 
 class Producer<Data = any> {
   static iterator = 0;
   id: string;
-  constructor(
-    private broker: Broker,
-    public topic: string,
-    private maxDeliveryAttempts = 10,
-    private timeout = 5000
-  ) {
-    if (topic == this.broker.deadLetterTopic) {
-      throw new Error("Cannot send to topic");
-    }
+  constructor(private broker: Broker, public topic: string) {
+    if (!topic) throw new Error("Invalid topic");
     this.id = `${Producer.iterator++}`;
-    this.broker.addProducer(this);
+    this.broker.register(this);
   }
+
   send(data: Data, metadata?: IMessageMetadata) {
-    let actualTopic = this.topic;
-    const message = new Message(data, metadata || {});
-
-    const { priority, attempts, timestamp } = message.metadata;
-    if (!timestamp) message.metadata.timestamp = Date.now();
-    if (attempts === 0) actualTopic = this.broker.deadLetterTopic;
-    else if (attempts) message.metadata.attempts = attempts - 1;
-    else message.metadata.attempts = this.maxDeliveryAttempts;
-
+    const message = new Message(data, metadata);
     try {
       const record = Message.encode(message);
-      this.broker.queues[actualTopic].enqueue(record, priority);
+      this.broker.queues[this.topic].enqueue(record, message.metadata.priority);
+
+      this.broker.logger?.log(
+        `[producer-${this.id}] ==> (${this.topic}): ${JSON.stringify(data)}`,
+        "INFO"
+      );
     } catch (e) {
       throw new Error("Failed to enqueue message");
     }
@@ -162,71 +204,99 @@ class Consumer<Data = any> {
   constructor(
     private broker: Broker,
     public topic: string,
-    public prefetch = 1,
-    private correlations?: ICorrelationRegistry
+    public prefetch = 1
   ) {
+    if (!topic) throw new Error("Invalid topic");
     this.id = `${Consumer.iterator++}`;
-    this.broker.addConsumer(this);
+    this.broker.register(this);
   }
+
   consume() {
     const messages: Message<Data>[] = [];
-    const now = Date.now();
-    // Batching
-    try {
-      while (
-        messages.length < this.prefetch &&
-        !this.broker.queues[this.topic].isEmpty()
-      ) {
-        const record = this.broker.queues[this.topic].peak()!;
-        const message = Message.decode<Data>(record);
-        const { correlationId, ttl, ttd } = message.metadata;
-        // Time-to-deliver
-        if (ttd && ttd > now) continue;
-        // Time-to-live
-        if (ttl && ttl > now) {
-          this.broker.queues[this.topic].dequeue();
-          continue;
-        }
-        // Consistent hashing
-        if (this.correlations && correlationId) {
-          const correlatedConsumerId = this.correlations.get(correlationId);
-          if (correlatedConsumerId && correlatedConsumerId !== this.id) {
-            continue;
-          }
-          this.correlations.set(correlationId, this.id);
-        }
+    let batchSize = 0;
 
-        this.broker.queues[this.topic].dequeue()!;
-        messages.push(message);
+    while (
+      batchSize < this.prefetch &&
+      !this.broker.queues[this.topic].isEmpty()
+    ) {
+      const record = this.broker.queues[this.topic].peek()!;
+      const message = Message.decode<Data>(record);
+      const { correlationId } = message.metadata;
+      if (message.isDelayed()) continue;
+      if (message.isExpired()) {
+        this.broker.queues[this.topic].dequeue();
+        message.metadata.attempts = this.broker.maxDeliveryAttempts; /////
+        this.nack([message]);
+        continue;
       }
-    } catch (e) {
-    } finally {
-      return messages;
+      if (correlationId && this.broker.isNotCorrelate(correlationId, this.id)) {
+        continue;
+      }
+
+      this.broker.queues[this.topic].dequeue()!;
+      messages.push(message);
+      batchSize++;
     }
+
+    const data = JSON.stringify(messages.map((r) => r.data));
+    this.broker.logger?.log(
+      `[consumer-${this.id}] <== (${this.topic}): ${data}`,
+      "INFO"
+    );
+
+    return messages;
   }
 
-  nack(messages: Message[]) {
+  async nack(messages: Message[]) {
     for (const message of messages) {
-      const { attempts = 0 } = message.metadata;
-      message.metadata.priority = -1;
-      message.metadata.attempts = attempts - 1;
-      this.sendTo<any>(topic, message.data, message.metadata);
+      let topic = this.topic;
+
+      if (message.isExceedAttempts(this.broker.maxDeliveryAttempts)) {
+        topic = this.broker.deadLetterTopic;
+      }
+
+      // Add delay before re-queuing (exponential backoff)
+      await wait(Math.min(2 ** message.metadata.attempts! * 1000, 30000));
+      const record = Message.encode(message);
+      this.broker.queues[topic].enqueue(record, message.metadata.priority);
+
+      const data = JSON.stringify(messages.map((r) => r.data));
+      this.broker.logger?.log(
+        `[consumer-${this.id}] ==> (${this.topic}): ${data}`,
+        "INFO"
+      );
     }
   }
 }
 
 /** Example */
 
-const correlations = new InMemoryCorrelationRegistry();
-const b = new Broker(InMemoryQueue);
+const b = new Broker(
+  InMemoryQueue,
+  new InMemoryCorrelationRegistry(),
+  new Logger()
+);
 
 const pub = new Producer(b, "test");
 pub.send({ test: 1 });
 
-const sub = new Consumer(b, "test", 1, correlations);
+const sub = new Consumer(b, "test", 1);
 const messages = sub.consume();
 sub.nack(messages);
 // b.unsubscribe(sub);
+// const brokerWithHeap = new Broker(HeapQueue);
+// const pub = new Producer(brokerWithHeap, "test");
+
+// // Send 100K messages with random priorities
+// for (let i = 0; i < 100_000; i++) {
+//   pub.send({ i }, { priority: Math.random() * 10 });
+// }
+
+// // Consume all
+// const sub = new Consumer(brokerWithHeap, "test");
+// while (!brokerWithHeap.queues["test"].isEmpty()) {
+//   sub.consume(); // ~3x faster than LinkedListQueue
+// }
 
 // type ILogger = {
 //   log(message: string, level: "INFO" | "WARN" | "DANGER"): void;
@@ -309,10 +379,3 @@ sub.nack(messages);
 
 // // Consumer.consume.subscribe.unsubscribe.pause
 // // Producer.send
-
-// this.logger?.log(
-//   `[producer-${1}] ==> (${topic}): ${JSON.stringify(data)}`,
-//   "INFO"
-// );
-// return this.broker.send(topic, message);
-// // 2021-08-27 13:09:50.349  INFO [producer-1] ===> (Purchases): key = htanaka value = t-shirts
